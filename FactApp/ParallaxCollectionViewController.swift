c416f08523328669c5634dc0604d4155b5841bce
ezequieloliveiralima <ezequieldeoliveiralima@gmail.com>
2015-05-29 15:17:50 -0300

    melhoramento arrumado

--- FactApp/ParallaxCollectionViewController.swift
+++ FactApp/ParallaxCollectionViewController.swift
@@ -161,7 +161,7 @@ class ParallaxCollectionViewController: UICollectionViewController {
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         //#warning Incomplete method implementation -- Return the number of items in the section
-        return data.count
+        return imagens.count
     }
     
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
